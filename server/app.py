61151d51c37d4e518985036cef2eb64e5bd80967
howsun <howsun7@gmail.com>
2022-11-05 18:53:49 -0700

    add user login endpoint

--- server/app.py
+++ server/app.py
@@ -2,14 +2,14 @@
 import os
 from flask import Flask, jsonify, send_from_directory, request
 from flask_cors import CORS
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import (
+    create_access_token,
+    get_jwt_identity,
+    jwt_required,
+    JWTManager,
+)
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
-
 
 '''Main wrapper for app creation'''
 app = Flask(__name__, static_folder='../build')
@@ -50,7 +50,21 @@ def user_signup():
     result = user_schema.dump(new_user)
     return jsonify(result), 201
 
+@app.route('/api/user/login', methods=['POST'])
+def user_login():
+    data = request.get_json()
+    user_email = data.get('email')
+    user_pwd = data.get('password')
 
+    if not User.instance_exists(email=user_email):
+        return jsonify(msg='user does not exist!'), 401
+
+    user = User.query.filter_by(email=user_email).first()
+    if user.password != user_pwd :
+        return jsonify(msg='wrong password! try again!'), 401
+
+    access_token = create_access_token(identity=user_email) 
+    return jsonify(token=access_token)
 
 
 @app.route('/api/items')
--- server/models.py
+++ server/models.py
@@ -3,7 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
+class CommonModelOperation:
+	@classmethod
+	def instance_exists(cls, **kwargs):
+		return cls.query.filter_by(**kwargs).count() > 0
+
+class User(db.Model, CommonModelOperation):
 	id = db.Column(db.Integer, primary_key=True)
 	email = db.Column(db.String(40), unique=True, nullable=False)
 	password = db.Column(db.String(80), nullable=False)
